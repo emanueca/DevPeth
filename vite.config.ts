3013b1643c5cef633d0ae623b3d9275a3e7052ae
Emanuel Ziegler Martins <emanuelziegler3@gmail.com>
2025-11-01 11:42:13 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -24,3 +24,35 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+
+//quando upar para o servidor
+
+/*
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      // MODIFICADO AQUI:
+      // Agora ele vai rodar na raiz (ex: http://localhost:8080/)
+      base: '/', 
+      
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+      },
+      plugins: [react()],
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
+});
+*/
